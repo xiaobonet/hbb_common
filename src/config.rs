532537461a13a51eeba969eaa2a573f961de7668
xiaobonet <admin@xiaobonet.cn>
2025-04-12 18:17:55 +0800

    修改为grup服务器地址

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["desk.qrup.fun"];
+pub const RS_PUB_KEY: &str = "Udu+aNnbt2ROcR8uqfyHzi4hvoCccewJrzHzHYvKiwE=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
